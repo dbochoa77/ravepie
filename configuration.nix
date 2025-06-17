65e73add8bf2d6dd75b84287fd45a84f5e55e452
Diego Ochoa <dbochoa77@proton.me>
2025-06-16 23:37:40 -0700

    Update configuration.nix
    clear old parts i did not need

--- configuration.nix
+++ configuration.nix
@@ -1,12 +1,8 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
       ./hardware-configuration.nix
     ];
 
@@ -17,12 +13,7 @@
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.hostName = "ravepie"; # Define your hostname.
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -65,7 +56,6 @@
   yt-dlp 
   git
   pkgs.xorg.xinit
-  spice-vdagent
   ];
 
   system.stateVersion = "25.05"; # Did you read the comment?
@@ -77,5 +67,4 @@
   services.xserver.enable = true;
 
   services.xserver.displayManager.startx.enable = true;
-  services.spice-vdagentd.enable = true;
 }
